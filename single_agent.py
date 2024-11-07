e6b22eb9af266afde70b97d88d7ef5c5b7c2b68e
bbhanja1809 <107783191+bbhanja1809@users.noreply.github.com>
2024-11-07 22:12:54 +0530

    Update single_agent.py

--- single_agent.py
+++ single_agent.py
@@ -4,6 +4,7 @@ import gym
 import numpy as np
 from stable_baselines.sac.policies import MlpPolicy
 from stable_baselines import SAC
+from metadrive.envs.gym_wrapper import createGymWrapper
 
 training_env = MetaDriveEnv(dict(
     num_scenarios=1000,
@@ -12,16 +13,9 @@ training_env = MetaDriveEnv(dict(
     random_agent_model=True,
     random_lane_num=True
 ))
+# gym_training_env = createGymWrapper(MetaDriveEnv)(env_config)
+gym_training_env = createGymWrapper(training_env)
 
-
-test_env = MetaDriveEnv(dict(
-    num_scenarios=200,
-    start_seed=0,
-    random_lane_width=True,
-    random_agent_model=True,
-    random_lane_num=True
-))
-
-model = SAC(MlpPolicy, env, verbose=1)
+model = SAC(MlpPolicy, gym_training_env, verbose=1)
 model.learn(total_timesteps=50000, log_interval=10)
 model.save("meta-drive")
